1ddded26452c94390bd29c45554caffe22dfed54
Rafael lima <rafaellrf09@gmail.com>
2020-06-08 13:04:58 -0300

    criando modelo de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,20 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Rafael Lima',
+      email: 'rafaellrf@gmail.com',
+      password_hash: 'asdgfdshgfsdhgd',
+    });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default routes;
