7ad9bdbd7e9a3cf7683ff33cd2487206cbc1cefa
Rafael lima <rafaellrf09@gmail.com>
2020-06-08 13:22:27 -0300

    criando rota de criação de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  try {
-    const user = await User.create({
-      name: 'Rafael Lima',
-      email: 'rafaellrf@gmail.com',
-      password_hash: 'asdgfdshgfsdhgd',
-    });
-    return res.json(user);
-  } catch (error) {
-    console.log(error);
-  }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
