81e36a4522fafd553bcf99018eba473d8e0a1eac
Rafael lima <rafaellrf09@gmail.com>
2020-06-10 11:52:17 -0300

    criação de upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
