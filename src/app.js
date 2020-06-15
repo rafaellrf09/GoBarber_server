828521eaece562f5f73fa6fa4cdaa355948a039b
Rafael lima <rafaellrf09@gmail.com>
2020-06-15 14:52:14 -0300

    configurando errors na aplicação com sentry e youch

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.app = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.app.use(Sentry.Handlers.requestHandler());
     this.app.use(bodyParser.json());
     this.app.use(
       '/files',
@@ -21,6 +30,15 @@ class App {
 
   routes() {
     this.app.use(routes);
+    this.app.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.app.use(async (err, req, res, nex) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
