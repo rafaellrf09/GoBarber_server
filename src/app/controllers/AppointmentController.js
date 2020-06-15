ad05086e589f824b7ca9735395e66b796677175f
Rafael lima <rafaellrf09@gmail.com>
2020-06-15 12:46:01 -0300

    mostrando campos virtuais

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -76,7 +76,7 @@ class ApointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
